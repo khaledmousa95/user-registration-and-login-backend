adcdabd63755473176a785c63079c630712d2b20
khaled mousa <45910686+khaledmousa95@users.noreply.github.com>
2022-11-15 02:26:11 +0400

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ mongooes.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
 app.use(express.json());
 // Route Middlewares
 app.use("/api/user", authRoute);
-app.use("/api/posts", postRoute);
+app.use("/api/posts", postRoute)
 
 app.listen(3000, () => console.log("the server is listing on 3000"));
